f117b9a181e0098a3509548f78b919af82e38d64
zobo24 <78169572+zobo24@users.noreply.github.com>
2023-07-20 20:59:51 +0200

    Add files via upload

---
+++ Kafic_cjenik/script.js
@@ -0,0 +1,9 @@
+///alert("Dobrodošli u caffe bar NINOMIL ");
+
+function preusmjeri(){
+    window.location.href = "vrste_pica.html";
+}
+
+function prethodna(){
+    window.location.href = "inddex.html";
+}
